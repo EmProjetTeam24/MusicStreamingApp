19ba32fb8cbcc1d492bdb62b1462d08d0bd4a752
unknown <pmlaforest@hotmail.com>
2019-02-05 13:43:12 -0800

    notification service updated to include API 26 and greater

--- app/src/main/java/project/pierremarclaforest/musicstreamingapp/NotificationService.java
+++ app/src/main/java/project/pierremarclaforest/musicstreamingapp/NotificationService.java
@@ -1,10 +1,13 @@
 package project.pierremarclaforest.musicstreamingapp;
 
 import android.app.IntentService;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 
 public class NotificationService extends IntentService {
     public static final int NOTIFICATION_ID = 5453;
@@ -20,9 +23,10 @@ public class NotificationService extends IntentService {
 
     public void createNotification()  {
 
-        //Create a notification builder
+        createChannel();
+
         NotificationCompat.Builder builder =
-                new NotificationCompat.Builder(this)
+                new NotificationCompat.Builder(this, "channel1")
                         .setSmallIcon(android.R.drawable.sym_def_app_icon)
                         .setContentTitle("Main title")
                         .setContentText("Content")
@@ -39,10 +43,22 @@ public class NotificationService extends IntentService {
                 PendingIntent.FLAG_UPDATE_CURRENT);
         builder.setContentIntent(actionPendingIntent);
 
-        //Issue the notification
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
         notificationManager.notify(NOTIFICATION_ID, builder.build());
+    }
+
+    private void createChannel() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("channel1", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
